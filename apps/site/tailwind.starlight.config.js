c97c90ae61a047c52027a10712dd74fa838b4959
Florian Lefebvre <contact@florian-lefebvre.dev>
2024-04-17 15:43:33 +0200

    feat: factorize and improve starlight styles

--- apps/site/tailwind.starlight.config.js
+++ apps/site/tailwind.starlight.config.js
@@ -1,40 +1,15 @@
+import { colors, fontFamily } from "./tailwind.config"
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
 			colors: {
-				primary: {
-					50: '#FEF6FA',
-					100: '#FCEDF5',
-					200: '#F9D7E8',
-					300: '#F5C1DB',
-					400: '#F0A3C9',
-					500: '#EB80B5',
-					600: '#E34F99',
-					700: '#AE1C66',
-					800: '#8D1752',
-					900: '#6E1240',
-					950: '#4B0C2B',
-				},
-				accent: {
-					50: '#FEF6FA',
-					100: '#FCEDF5',
-					200: '#F9D7E8',
-					300: '#F5C1DB',
-					400: '#F0A3C9',
-					500: '#EB80B5',
-					600: '#E34F99',
-					700: '#AE1C66',
-					800: '#8D1752',
-					900: '#6E1240',
-					950: '#4B0C2B',
-				},
+				primary: colors.primary,
+				accent: colors.primary,
 			},
-			fontFamily: {
-				sans: ['"Inter Variable"'],
-				heading: ['"Dosis Variable"'],
-			},
+			fontFamily,
 		},
 	},
 	plugins: [require('@astrojs/starlight-tailwind')()],
