80d88361f62055ff2f2439276514b52f597d6a05
Alexander Niebuhr <alexander@nbhr.io>
2025-12-21 18:56:08 +0100

    update configs

--- astro.config.ts
+++ astro.config.ts
@@ -6,31 +6,33 @@ import tailwindcss from '@tailwindcss/vite';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
-    build: {
-        inlineStylesheets: 'auto',
-    },
-    scopedStyleStrategy: 'attribute',
-    base: '/',
-    devToolbar: {
-        enabled: true,
-    },
-    site: 'https://astrolicious.dev/',
-    prefetch: {
-        prefetchAll: false,
-        defaultStrategy: 'hover',
-    },
-    experimental: {
-        clientPrerender: true,
-    },
-    integrations: [mdx()],
-    output: 'static',
-    adapter: cloudflare(),
-    vite: {
-      build: {
-          minify: false,
-          cssMinify: false,
-      },
+	build: {
+		inlineStylesheets: 'auto',
+	},
+	scopedStyleStrategy: 'attribute',
+	base: '/',
+	devToolbar: {
+		enabled: true,
+	},
+	site: 'https://astrolicious.dev/',
+	prefetch: {
+		prefetchAll: false,
+		defaultStrategy: 'hover',
+	},
+	experimental: {
+		clientPrerender: true,
+	},
+	integrations: [mdx()],
+	output: 'static',
+	adapter: cloudflare({
+		imageService: 'cloudflare-binding',
+	}),
+	vite: {
+		build: {
+			minify: false,
+			cssMinify: false,
+		},
 
-      plugins: [tailwindcss()],
-    },
+		plugins: [tailwindcss()],
+	},
 });
